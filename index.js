8193d4ff4c2171e684ad61731280b3f85f1ba12a
Devin <dbelle722@gmail.com>
2024-01-31 18:15:05 -0500

    Prompt for generating SVG file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-//Wee need to add the files that are required
+//We need to add the files that are required
 const inquirer = require('inquirer');
 const fs = require('fs');
+//Included the shapes module for the questions
+const { Shape, Triangle, Circle, Square } = require('./test/shapes');
 
-//Below are the questions for inputting the shapes
+//Below are the questions for inputing the shapes
 const questions = [
     {
         type: 'list',
@@ -27,3 +29,52 @@ const questions = [
         message: 'What kind of background color do you want for the shape (enter name of color or hashtag)?'
     },
 ];
+
+
+//created a prompt for inquier to include the shapes and colors
+inquirer.prompt(questions).then((answers) => {
+    //destructured class to link to the user answers
+    const { text, textColor, shape, shapeColor } = answers;
+    //links the created variable with our created shape class
+    const shapeObj = new Shape();
+    //empty string for user input
+    let svgElement = '';
+    //sets the color of the shape into the created shape object
+    shapeObj.setColor(shapeColor);
+    //created a switch statement for each shape of choice. 
+    switch (shape){
+        case 'Triangle':
+            const triangle = new Triangle();
+            triangle.setColor(shapeColor);
+            svgElement = triangle.render();
+            break;
+        case 'Circle':
+            const circle = new Circle();
+            circle.setColor(shapeColor);
+            svgElement = circle.render();
+            break;
+        case 'Square':
+            const square = new Square();
+            square.setColor(shapeColor);
+            svgElement = square.render();
+            break;
+    }
+
+    //if statement for choosing either a triangle or square with svg elements
+    let x = 150, y = 120;
+    if (shape === 'Triangle') {
+        y = 135;
+    } else if (shape === 'Square') {
+        y = 145;
+    }
+    //production of the final logo, texy and textColor are the user choices int eh prompt above.
+    const finalSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+        ${svgElement}
+        <text x="${x}" y="${y}" fill="${textColor}" font-size="50" text-anchor="middle">${text}</text>
+        </svg>`;
+        //creates the file
+        fs.writeFileSync('logo.svg', finalSvg);
+        //what is written in the console.log
+        console.log('Generated logo.svg');
+});
+
